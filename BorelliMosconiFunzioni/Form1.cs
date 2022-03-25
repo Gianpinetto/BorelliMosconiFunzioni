0e7ea1b43372361954ffe2980451ce699c12c161
Marco Borelli <marco05.borelli@gmail.com>
2022-03-25 10:55:06 +0100

    prima versione e prova di diverse lbrerie grafiche

--- BorelliMosconiFunzioni/Form1.cs
+++ BorelliMosconiFunzioni/Form1.cs
@@ -16,5 +16,32 @@ namespace BorelliMosconiFunzioni
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pens.Red,0,0,100,100);
+            e.Graphics.DrawLine(Pens.Red, 100, 100, 200,300);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
     }
 }
