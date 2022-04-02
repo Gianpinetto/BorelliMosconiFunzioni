6d11c5a031de6f97c72487798d91f9ff6ec6171a
Gianpinetto <marco05.borelli@gmail.com>
2022-04-02 10:52:29 +0200

    prima versione grafico

--- BorelliMosconiFunzioni/Form1.Designer.cs
+++ BorelliMosconiFunzioni/Form1.Designer.cs
@@ -61,6 +61,7 @@ namespace BorelliMosconiFunzioni
             this.button1.TabIndex = 2;
             this.button1.Text = "CALC";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
