08353d9d67ed2333ed9cebe64e302f92ae4ae3f0
Gianpinetto <marco05.borelli@gmail.com>
2022-04-14 17:43:22 +0200

    aggiunta degli assi. colore nero degli assi. siamo felici.

--- BorelliMosconiFunzioni/Form1.cs
+++ BorelliMosconiFunzioni/Form1.cs
@@ -40,6 +40,8 @@ namespace BorelliMosconiFunzioni
         PlotModel model = new PlotModel();
         
         FunctionSeries fs = new FunctionSeries();
+        FunctionSeries x = new FunctionSeries();
+        FunctionSeries y = new FunctionSeries();
         FunctionSeries fx1 = new FunctionSeries();
         FunctionSeries fx2 = new FunctionSeries();
         FunctionSeries fx3 = new FunctionSeries();
@@ -77,15 +79,29 @@ namespace BorelliMosconiFunzioni
                 Controls.Add(pv);
 
 
-                pv.Model = new PlotModel { Title = "CIAO" };
+                pv.Model = new PlotModel { Title = $"GRAFICO DELLA FUNZIONE {textBox2.Text}" };
 
                 pv.Model.Axes.Add(xAxis);//mio
 
 
                 pv.Model.Axes.Add(yAxis); //aggiungiamo gli assi
+
+
+                x.Points.Add(new DataPoint(0, -10000));
+                x.Points.Add(new DataPoint(0, 10000));
+
+                OxyColor ciao = new OxyColor(); //trovato abbastanza a caso senza stack overflow
+                ciao = OxyColor.FromArgb(255,0,0,0);
+                x.Color = ciao;
+                pv.Model.Series.Add(x);
+
+                y.Points.Add(new DataPoint(-10000,0));
+                y.Points.Add(new DataPoint(10000,0));
+                y.Color = ciao;
+                pv.Model.Series.Add(y);
                 for (int i = 1; i < range; i++)
                 {
-                    if (condizioni[i]==false) //se non è òa condizione di esistenza interrompo linea
+                    if (condizioni[i]==false) //se non è la condizione di esistenza interrompo linea
                         fs.Points.Add(new DataPoint(coordinate[0, i], coordinate[1, i]));
                     else
                         fs.Points.Add(DataPoint.Undefined);                    
@@ -93,15 +109,15 @@ namespace BorelliMosconiFunzioni
 
                 pv.Model.Series.Add(fs);
 
-                pv.Model.Axes[1].Minimum = -10;  //[1]=X  [0] = Y
-                pv.Model.Axes[1].Maximum = 10; 
-                pv.Model.Axes[0].Minimum = -10; 
-                pv.Model.Axes[0].Maximum = 10;
+                pv.Model.Axes[1].Minimum = -100;  //[1]=Y  [0] = X
+                pv.Model.Axes[1].Maximum = 100; 
+                pv.Model.Axes[0].Minimum = -100; 
+                pv.Model.Axes[0].Maximum = 100;
 
-                pv.Model.Axes[0].AbsoluteMinimum = coordinate[0, 0];
-                pv.Model.Axes[0].AbsoluteMaximum = coordinate[0, 9999];
-                pv.Model.Axes[1].AbsoluteMinimum = ymin;
-                pv.Model.Axes[1].AbsoluteMaximum = ymax;
+                pv.Model.Axes[0].AbsoluteMinimum = coordinate[0, 0]-0.25;
+                pv.Model.Axes[0].AbsoluteMaximum = coordinate[0, (coordinate.GetLength(1)-1)]+0.25;
+                pv.Model.Axes[1].AbsoluteMinimum = ymin-0.25;
+                pv.Model.Axes[1].AbsoluteMaximum = ymax+0.25;
 
             }
         }
@@ -120,7 +136,7 @@ namespace BorelliMosconiFunzioni
                 funzione = backup;
                 try
                 {
-                    Risoluzione(funzione, coordinate, contatore, ref x, 1, ref ymin, ref ymax, aumentoX);
+                    Risoluzione(ref funzione, coordinate, contatore, ref x, 1, ref ymin, ref ymax, aumentoX);
                 }
                 catch
                 {
@@ -128,10 +144,9 @@ namespace BorelliMosconiFunzioni
                 }
 
                 xCiao =x- aumentoX;
-                funzione = backup;
                 if (condizioni[contatore] != true)
                 {
-                    Risoluzione(funzione, coordinate, contatore, ref xCiao, 0, ref ymin, ref ymax, aumentoX);
+                    Risoluzione(ref funzione, coordinate, contatore, ref xCiao, 0, ref ymin, ref ymax, aumentoX);
                     controllo = 1;
                 }
                 contatore++;
@@ -172,22 +187,25 @@ namespace BorelliMosconiFunzioni
             }
             return RisFin;
         }
-        public static void Risoluzione(string funzione, double[,] coordinata, int contatore, ref double x, int condizione, ref double ymin, ref double ymax, double aumentoX)
+        public static void Risoluzione(ref string funzione, double[,] coordinata, int contatore, ref double x, int condizione, ref double ymin, ref double ymax, double aumentoX)
         {
             double y = 0;
-            string xStringa = "";
-            xStringa = Convert.ToString(x);
-            xStringa = Regex.Replace(xStringa, "[,]", "."); //converto la virgola in punto perchè sennò da errore
-
-            for (int i = 0; i < funzione.Length; i++)
+            if (condizione == 1)
             {
-                if (funzione.Substring(i, 1).ToUpper() == "X")
+                string xStringa = "";
+                xStringa = Convert.ToString(x);
+                xStringa = Regex.Replace(xStringa, "[,]", "."); //converto la virgola in punto perchè sennò da errore
+
+                for (int i = 0; i < funzione.Length; i++)
                 {
-                    funzione = funzione.Remove(i, 1); //tolgo la x
-                    funzione = funzione.Insert(i, $"*({xStringa})"); //rimpiazzo con "*"+ il numero al momento
+                    if (funzione.Substring(i, 1).ToUpper() == "X")
+                    {
+                        funzione = funzione.Remove(i, 1); //tolgo la x
+                        funzione = funzione.Insert(i, $"*({xStringa})"); //rimpiazzo con "*"+ il numero al momento
+                    }
                 }
             }
-
+            
             Expression expr = new Expression(funzione);
             x += aumentoX;
             var value = expr.Eval(); //calcolo il valore della nuova espressione
@@ -198,7 +216,7 @@ namespace BorelliMosconiFunzioni
             if (y > ymax)
                 ymax = y;
 
-            if (condizione != 1) //se non è la volta in cui entro nel ciclo solo per controllare le condizioni
+            if (condizione == 0) //se non è la volta in cui entro nel ciclo solo per controllare le condizioni
             {
                 coordinata[0, contatore] = x - aumentoX;
                 coordinata[1, contatore] = y;
